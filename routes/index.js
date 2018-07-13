648701483ce9dd73b84423e7f2573192910b2607
tophski <talkingtikihead@gmail.com>
2018-07-13 11:09:42 -0600

    Working index page with partial html coded posts

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,16 @@
+let fs = require("fs");
+let path = require("path");
 module.exports = function(app) {
+    let postPaths = fs.readdirSync(path.join(__dirname, "../public/posts"));
+    let posts = [];
+    for(let i = 0; i < postPaths.length; i++){
+        let file = fs.readFileSync(path.join(__dirname, '../public/posts', postPaths[i]), 'utf8');
+        posts[i] = file;
+        console.log(file);
+    }
+
+
     app.get('/', function (req, res, next) {
-        res.render('index', {title: 'Express'});
+        res.render('index', {title: 'Express', posts: posts});
     });
 };
